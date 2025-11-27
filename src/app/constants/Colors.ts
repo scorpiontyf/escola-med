0bac306c7f2f42ff44ee17a975f984f3e1fd9b85
gcanechia <gcanechia@provertec.com.br>
2025-11-27 19:41:39 -0300

    estrutura inicial

--- src/app/constants/Colors.ts
+++ src/app/constants/Colors.ts
@@ -1,7 +1,7 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
-export default {
+export const colors = {
   light: {
     text: '#000',
     background: '#fff',
@@ -16,4 +16,19 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  gray: {
+    100: "#F4F4F5",
+    200: "#E4E4E7",
+    300: "#D4D4D8",
+    400: "#A1A1AA",
+    500: "#71717A",
+    600: "#52525B",
+    800: "#27272A",
+    900: "#18181B",
+    950: "#09090B",
+  },
+  green: {
+    300: "#2DD4BF",
+    900: "#042F2E"
+  }
 };
